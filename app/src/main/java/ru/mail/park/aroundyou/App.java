ebe2ab8c57fdd41972f2ffaa1827d0f505f061fe
Sovianum <aklyukvin@yandex.com>
2017-11-14 09:44:21 +0300

    add strict mode

---
+++ app/src/main/java/ru/mail/park/aroundyou/App.java
@@ -0,0 +1,24 @@
+package ru.mail.park.aroundyou;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectAll()
+                .penaltyLog()
+                .build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                .detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects()
+                .penaltyLog()
+                .penaltyDeath()
+                .build());
+
+        super.onCreate();
+    }
+}
